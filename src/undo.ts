51011d14388a13095a18d477634e396b7294d4f2
Tim Perry <pimterry@gmail.com>
2020-11-16 15:17:57 +0100

    Initial implementation

--- src/undo.ts
+++ src/undo.ts
@@ -1,1 +1,50 @@
-export const undo = undefined;
+import * as mobx from 'mobx';
+
+export function trackUndo<T>(
+    readObservable: () => T,
+    setObservable: (value: T) => void
+) {
+    let isDisposed = false;
+
+    const undoStack: T[] = [readObservable()];
+    let undoPointer = undoStack.length - 1; // Always points to the current value
+
+    let stopTrackingChanges: () => void;
+    const trackChanges = () => {
+        stopTrackingChanges = mobx.reaction(readObservable, (newValue) => {
+            if (isDisposed) throw new Error('Undo already disposed');
+
+            undoPointer += 1;
+            undoStack[undoPointer] = newValue;
+            undoStack.length = undoPointer + 1; // On change, trim any trailing redo's
+        });
+    };
+
+    trackChanges();
+
+    return {
+        undo: mobx.action(() => {
+            if (isDisposed) throw new Error('Undo already disposed');
+            undoPointer -= 1;
+
+            // We have to make this change without our undo stack including it, but
+            // whilst still letting other external viewers observe this as normal.
+            stopTrackingChanges();
+            setObservable(undoStack[undoPointer]);
+            trackChanges();
+        }),
+        redo: mobx.action(() => {
+            if (isDisposed) throw new Error('Undo already disposed');
+            if (undoPointer >= undoStack.length - 1) return; // No redos available
+            undoPointer += 1;
+
+            stopTrackingChanges();
+            setObservable(undoStack[undoPointer]);
+            trackChanges();
+        }),
+        dispose: () => {
+            stopTrackingChanges();
+            isDisposed = true;
+        }
+    }
+}
--- test/undo.spec.ts
+++ test/undo.spec.ts
@@ -1,10 +1,94 @@
+import * as mobx from 'mobx';
+
 import { expect } from 'chai';
 
-import { undo } from '../src/undo';
+import { trackUndo } from '../src/undo';
 
-describe("Mobx shallo wundo", () => {
-    it("can run a test", () => {
-        expect(undo).to.equal(undefined);
-        expect(1 + 1).to.equal(2);
+// Convenient set-as-an-action
+const set = mobx.action(<T>(obsv: mobx.IObservableValue<T>, value: T) => {
+    obsv.set(value);
+});
+
+describe("Mobx undo", () => {
+    it("can undo initial change", () => {
+        const obsv = mobx.observable.box<number>(123);
+        const undo = trackUndo(() => obsv.get(), (v) => obsv.set(v));
+
+        set(obsv, 456);
+        expect(obsv.get()).to.equal(456);
+
+        undo.undo();
+        expect(obsv.get()).to.equal(123);
+    });
+
+    it("can repeatedly undo changes", () => {
+        const obsv = mobx.observable.box<number>(123);
+        const undo = trackUndo(() => obsv.get(), (v) => obsv.set(v));
+
+        set(obsv, 456);
+        set(obsv, 789);
+        expect(obsv.get()).to.equal(789);
+
+        undo.undo();
+        expect(obsv.get()).to.equal(456);
+
+        undo.undo();
+        expect(obsv.get()).to.equal(123);
+    });
+
+    it("can repeatedly undo changes within an action", () => {
+        const obsv = mobx.observable.box<number>(123);
+        const undo = trackUndo(() => obsv.get(), (v) => obsv.set(v));
+
+        set(obsv, 456);
+        set(obsv, 789);
+        expect(obsv.get()).to.equal(789);
+
+        mobx.runInAction(() => {
+            undo.undo();
+            undo.undo();
+        });
+
+        expect(obsv.get()).to.equal(123);
+    });
+
+    it("can redo changes", () => {
+        const obsv = mobx.observable.box<number>(123);
+        const undo = trackUndo(() => obsv.get(), (v) => obsv.set(v));
+
+        set(obsv, 456);
+        set(obsv, 789);
+        expect(obsv.get()).to.equal(789);
+
+        undo.undo();
+        expect(obsv.get()).to.equal(456);
+
+        undo.redo();
+        expect(obsv.get()).to.equal(789);
+    });
+
+    it("can resets redo stack after changes", () => {
+        const obsv = mobx.observable.box<number>(123);
+        const undo = trackUndo(() => obsv.get(), (v) => obsv.set(v));
+
+        set(obsv, 456);
+        set(obsv, 789);
+        expect(obsv.get()).to.equal(789);
+
+        undo.undo();
+        expect(obsv.get()).to.equal(456);
+
+        set(obsv, 0);
+        expect(obsv.get()).to.equal(0);
+
+        undo.redo(); // No-op
+        expect(obsv.get()).to.equal(0);
+    });
+
+    it("stops observing after dispose", () => {
+        const obsv = mobx.observable.box<number>(123);
+        const undo = trackUndo(() => obsv.get(), (v) => obsv.set(v));
+        undo.dispose();
+        expect(mobx.getObserverTree(obsv).observers).to.equal(undefined);
     });
 });
